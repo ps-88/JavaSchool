ff034559be5635717fec9f73b63691191e3f93c7
Pasha <example@mail.com>
2020-11-08 20:41:00 +0200

    lambda

--- src/main/java/task03Lambda/Main.java
+++ src/main/java/task03Lambda/Main.java
@@ -4,15 +4,16 @@ import lombok.SneakyThrows;
 
 import javax.swing.*;
 import java.util.List;
+import java.util.function.Consumer;
 
 import static java.util.Arrays.asList;
 
 public class Main {
 
     @SneakyThrows
-    public static <T> void forEachWithDelay (List <T> list, int delay, Action <T> action) {
+    public static <T> void forEachWithDelay (List <T> list, int delay, Consumer <T> action) {
         for (T t : list) {
-            action.doAction(t);
+            action.accept(t);
             Thread.sleep(delay);
         }
     }
@@ -20,7 +21,7 @@ public class Main {
     public static void main(String[] args) {
 
         List <Integer> nums = asList(34,46,76,97,3,2);
-        forEachWithDelay(nums,50,x -> System.out.println(x));
+        forEachWithDelay(nums,250,x -> System.out.println(x));
 
     }
 }
